61722ebd98f469e5e2b2431dbaef8fa470dba74d
Shamitsu212 <darklin314@gmail.com>
2026-06-14 17:46:23 +0300

    feat: Добавлен просмотр через gh-pages

--- react_ToDo/src/main.tsx
+++ react_ToDo/src/main.tsx
@@ -9,7 +9,7 @@ import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom';
 
 createRoot(document.getElementById('root')!).render(
-  <BrowserRouter basename="/React_TodoList">
+  <BrowserRouter basename="/React_TodoList/">
     <App />
   </BrowserRouter>
 )
